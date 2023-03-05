cbe232067fb18a6c68416294a69aa9f386f201e2
Monica Murray <Monica.rickerby@gmail.com>
2023-03-05 09:34:03 +0000

    Add Success message to comment post in PostDetail, set Debug to False, add X_FRAME_Options

--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,7 @@ from django.views import generic, View
 from django.http import HttpResponseRedirect
 from .models import Post
 from .forms import CommentForm
+from django.contrib import messages
 
 # Create your views here.
 
@@ -53,6 +54,7 @@ class PostDetail(View):
             comment = comment_form.save(commit=False)
             comment.post = post
             comment.save()
+            messages.success(request, 'Thank you for your comment.')
         else:
             comment_form = CommentForm()
         
